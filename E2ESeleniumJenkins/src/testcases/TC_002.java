4993c9ebf859c433ad873f0e266c23cc09839e01
vinod <pmvinodkumar@gmail.com>
2018-10-10 09:10:11 +0530

    Created auomation project

---
+++ E2ESeleniumJenkins/src/testcases/TC_002.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+
+	@Test
+	public void bmet()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.shop.thetestingworld.com/");
+		driver.findElement(By.name("search")).sendKeys("IPhone");
+		driver.findElement(By.xpath("//div[@id='search']/span/button[@class='btn btn-default btn-lg']")).click();
+		driver.quit();
+	}
+	
+}
